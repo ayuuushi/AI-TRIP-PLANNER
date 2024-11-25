0202770d8570ad111f2637fdbc3caa1280823b67
Ayushi Singh <ayushisinghapple@gmail.com>
2024-11-25 23:13:50 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,25 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter } from 'react-router-dom'
+import { RouterProvider } from 'react-router'
+import CreatTrip from './create-trip'
+import CreateTrip from './create-trip'
+
+const router=createBrowserRouter([
+  {
+    path:'/',
+    element:<App/>
+  }
+  {
+    path:'/create-trip',
+    element:<CreateTrip/>
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
